72c1e0906fbc82c179c80a3464dd75aa48975a45
Avrizal Setyo Aji Nugroho <151628376+RzlExcel@users.noreply.github.com>
2026-06-02 00:47:34 +0700

    Delete univent 2.0 directory

--- univent 2.0/resources/views/vendor/mail/html/header.blade.php
+++
@@ -1,9 +0,0 @@
-@props(['url'])
-<tr>
-<td class="header">
-<a href="{{ $url }}" style="display: inline-block;">
-    {{-- Menggunakan url() agar menghasilkan alamat lengkap http://domain.com/.. --}}
-    <img src="{{ url('images/univent-logo2.png') }}" alt="Univent Logo" style="height: 50px; width: auto; max-width: 100%;">
-</a>
-</td>
-</tr>
--- univent 2.0/routes/web.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-require __DIR__.'/modules/dashboard.php';
-require __DIR__.'/modules/auth.php';
-require __DIR__.'/modules/profile.php';
-require __DIR__.'/modules/contact.php';
-require __DIR__.'/modules/event.php';
-require __DIR__.'/modules/admin.php';
